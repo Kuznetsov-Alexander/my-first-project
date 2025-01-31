aff2b17be6231c9782160c337e1b47db8a743a24
Alexander <sashakuznetsov64@gmail.com>
2025-01-31 17:42:20 +0300

    Добавил подкласс для Car

--- main.py
+++ main.py
@@ -11,8 +11,31 @@ class Car:
         print(f"Год выпуска: {self.year}")
         print(f"Цвет: {self.color}")
 
+class ElectricCar(Car):
+    def __init__(self, brand, model, year, color, battery_capacity):
+        super().__init__(brand, model, year, color)
+        self.battery_capacity = battery_capacity
+    def display_info(self):
+        print(f"Марка: {self.brand}")
+        print(f"Модель: {self.model}")
+        print(f"Год выпуска: {self.year}")
+        print(f"Цвет: {self.color}")
+        print(f"Заряд: {self.battery_capacity}")
 
-my_car = Car("Toyota", "Camry", 2020, "red")
+
+my_car_1 = Car("Toyota", "Camry", 2020, "red")
+my_car_2 = ElectricCar("Tesla", "Model S", 2023, "blue", 100)
+
+my_car_1.display_info()
+my_car_2.display_info()
 
 
-my_car.display_info()
+
+
+
+
+
+
+
+
+
