e3ae158f7b886e4e2ac475fc4f10d47c44729df4
Alexander <sashakuznetsov64@gmail.com>
2025-02-01 19:46:13 +0300

    Добавил геттер и сеттер для year

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ class Car:
     def __init__(self, brand, model, year, color):
         self.brand = brand
         self.model = model
-        self.year = year
+        self.__year = year
         self.color = color
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, value):
+        if value > 1900:
+            self.__year = value
 
     def display_info(self):
         print(f"Марка: {self.brand}")
@@ -25,6 +34,7 @@ class ElectricCar(Car):
 
 my_car_1 = Car("Toyota", "Camry", 2020, "red")
 my_car_2 = ElectricCar("Tesla", "Model S", 2023, "blue", 100)
+
 
 my_car_1.display_info()
 my_car_2.display_info()
